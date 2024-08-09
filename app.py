4d8affee4df3a6f2a6c41f6afa950e4f6c19f626
phdaccache <pedrodaccache@gmail.com>
2024-08-09 00:35:45 -0300

    tutorial

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ from configure import make_backup
 
 st.title("Notion Backup")
 
+with st.expander("See tutorial"):
+    st.write("On Notion, click on the '...' icon at the top right corner of the page you want to backup and click on 'export'.")
+    st.write("Make sure to export it as HTML and to mark every option:")
+    st.image("image.png")
+    st.write("With the zip file you just downloaded, upload it below and click on 'Make Backup'.")
+    st.write("Once finished, download the new zip file and extract it where you want it.")
+    st.write("To open your page locally, just click on '0.html':")
+    st.image("image2.png")
+
 uploaded_file = st.file_uploader("Choose a file",type="zip")
 
 if st.button("Make Backup"):
