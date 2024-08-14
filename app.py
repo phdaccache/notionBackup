cd1ef65f346b2e6c03fe194084fc39356933ac09
phdaccache <pedrodaccache@gmail.com>
2024-08-14 19:45:16 -0300

    design changes

--- app.py
+++ app.py
@@ -3,14 +3,6 @@ import backup as bp
 
 st.title("Notion Backup")
 
-with st.expander("See tutorial"):
-    st.write("On Notion, click on the '...' icon at the top right corner of the page you want to backup and click on 'export'.")
-    st.write("Make sure to export it as HTML and to mark every option:")
-    st.image("image.png")
-    st.write("With the zip file you just downloaded, upload it below and click on 'Make Backup'.")
-    st.write("Once finished, download the new zip file and extract it where you want it.")
-    st.write("To open your page locally, just click on '0.html':")
-    st.image("image2.png")
 
 uploaded_file = st.file_uploader("Choose a file",type="zip")
 
@@ -28,7 +20,19 @@ if st.button("Make Backup"):
             mime="application/zip"
         )
 
+st.info("Remember to refresh the page to make another backup.")
+
 st.write("---")
+
+with st.expander("See tutorial"):
+    st.write("On Notion, click on the '...' icon at the top right corner of the page you want to backup and click on 'export'.")
+    st.write("Make sure to export it as HTML and to mark every option:")
+    st.image("image.png")
+    st.write("With the zip file you just downloaded, upload it below and click on 'Make Backup'.")
+    st.write("Once finished, download the new zip file and extract it where you want it.")
+    st.write("To open your page locally, just click on '0.html':")
+    st.image("image2.png")
+
 with st.expander("Zip too big?"):
     st.write("Download the script and run it locally!")
     st.write("Make sure you have your zipped file from Notion and the python script in the same directory, just like this:")
