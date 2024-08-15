005324fdddde42390ba3d3ebf13bfab924bef77f
phdaccache <pedrodaccache@gmail.com>
2024-08-14 21:50:47 -0300

    fix

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import backup as bp
 
 st.title("Notion Backup")
 
@@ -9,10 +8,10 @@ st.write("Make sure to export it as HTML and to mark every option:")
 st.image("image.png")
 st.write("Now, you have to download the python script and run it locally.")
 st.write("Make sure you have your zipped file from Notion and the python script in the same directory, just like this:")
-st.image("image3.png")
+st.image("image2.png")
 st.write("Then, just run the backup.py script and follow the instructions in the terminal.")
 st.write("To open your page locally, just click on '0.html':")
-st.image("image2.png")
+st.image("image3.png")
 
 st.write("---")
 
